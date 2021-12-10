5cf88ff49b841cadd7f5c9d9659194a0d9518178
Alireza <alireza81.amiri@gmail.com>
2021-12-11 00:02:41 +0330

    inam tamoom shod

---
+++ javascript.js
@@ -0,0 +1,8 @@
+function Begir() {
+    document.getElementById("form").submit();
+  }
+  function payam(){
+      var a = confirm( "ایااز ارسال خود مطمئنید؟");
+      if(a == true) {alert("SUBMITED!")};
+      if(a == true) {Begir()};
+  }
